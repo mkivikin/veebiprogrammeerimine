a6bcf3f4771054aded709de0bc3126fb18dee992
Marek Kivikink <mkivikin@users.noreply.github.com>
2017-09-14 10:47:04 +0300

    lisatud esimene php jutt

---
+++ esimene.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>
+		Marek
+	</title>
+</head>
+<body>
+	<h1>Marek</h1>
+	<p>See veebileht on loodud õppetöö raames ning ei 
+	sisalda mingisugust tõsiseltvõetavat sisu!</p>
+	<p>Kirjutasin siia veits midagi kodus ka veel.
+	<br>Tegelt sa oled normaalne vend
+	<br>But why male models?<br>
+	<img src="http://greeny.cs.tlu.ee/~kivimare/zoolander.jpg" alt="malemodels">
+	<br>
+	<?php
+		echo "Algas PHP õppimine";
+	?>
+</body>
+</html>
