b23e65396517ae62855810adfa67b4216df45428
Marek Kivikink <mkivikin@users.noreply.github.com>
2017-09-21 11:35:25 +0300

    Lisatud pildimassiiv, failitüübi kontroll ja random pildi genereerimine

---
+++ Tund3/avaleht.php
@@ -0,0 +1,24 @@
+<?php
+	//muutujad
+	$myName = "Marek";
+	$mySurname = "Kivikirbe";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>
+		Marek
+	</title>
+</head>
+<body>
+	<h1><?php  echo $myName . " " . $mySurname;?></h1>
+	<p>See veebileht on loodud õppetöö raames ning ei 
+	sisalda mingisugust tõsiseltvõetavat sisu!</p>
+	<p>Kirjutasin siia veits midagi kodus ka veel.</p>
+	<p> Meie õpime <a href="http://www.tlu.ee">Tallinna Ülikoolis</a> </p>
+	<p>Minu esimene php leht on <a href="../esimene.php">siin</a></p>
+	<p>Minu sõbra Tuule leht on <a href="../../../~poldtuul/veebiprogrammeerimine/tund3/avaleht.php/">siin</a>
+	<p>Pilte näeb <a href="foto.php">siin</a></p>
+</body>
+</html>
