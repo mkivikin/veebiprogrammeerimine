24beedc46e62b6656adc057a9ae8fcf98a0ce86b
Marek Kivikink <mkivikin@users.noreply.github.com>
2017-09-21 10:23:41 +0300

    Lisatud massiivid, for tsükkel ja esimene vorm.

--- esimene.php
+++ esimene.php
@@ -1,3 +1,54 @@
+<?php
+	//muutujad
+	$myName = "Marek";
+	$mySurname = "Kivikirbe";
+	$monthNamesET = ["Jaanuar", "Veebruar", "Märts", "Aprill", "Mai", "Juuni",
+	"Juuli", "August", "September", "Oktoober", "November", "Detsember"];
+	
+	//var_dump ($monthNamesET);
+	//var_dump($_POST);
+	//echo $_POST["birthYear"];
+	//echo $monthNamesET[1];
+	$myBirthYear;
+	$ageNotice = "";
+	if  (isset($_POST["birthYear"]) and $_POST["birthYear"] != 0) {
+		$myBirthYear = $_POST["birthYear"];
+		$myAge = date("Y") - $_POST["birthYear"];
+		$ageNotice = "<p>Te olete umbkaudu " .$myAge ." Aastat vana.</p>";
+		
+		$ageNotice .= "<p>Te olete elanud järgnevatel aastatel: </p> <ul>";
+		for ($i = $myBirthYear; $i <= date("Y"); $i ++) {
+			$ageNotice .="<li>" .$i ."</li>";
+		}			
+		$ageNotice .="</ul>";
+	}
+	
+	$monthNow = $monthNamesET[date ("n") -1];
+	
+	//Hindan päevaosa
+	$hourNow = date("H");
+	$partOfDay ="";
+	
+	if ($hourNow < 8) {
+		$partOfDay = "Varajane Hommik";
+	}
+	if ($hourNow >= 8 and $hourNow < 16) {
+		$partOfDay = "Koolipäev";
+	}
+	
+	else {
+		$partOfDay = "Vabaaeg";
+	}
+	
+	//echo $partOfDay;
+	
+	/* 
+	for ($i = 0; $i <5; $i ++) {
+		echo "Ha";
+	}
+	*/
+	
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,16 +58,33 @@
 	</title>
 </head>
 <body>
-	<h1>Marek</h1>
+	<h1><?php  echo $myName . " " . $mySurname;?></h1>
 	<p>See veebileht on loodud õppetöö raames ning ei 
 	sisalda mingisugust tõsiseltvõetavat sisu!</p>
 	<p>Kirjutasin siia veits midagi kodus ka veel.
 	<br>Tegelt sa oled normaalne vend
-	<br>But why male models?<br>
+	<br>
 	<img src="http://greeny.cs.tlu.ee/~kivimare/zoolander.jpg" alt="malemodels">
 	<br>
 	<?php
-		echo "Algas PHP õppimine";
+		echo "<p>Algas PHP õppimine</p>";
+		echo "<p>Täna on: ";
+		echo date("d. ") . $monthNow. date(" Y") .", Kell oli lehe avamise hetkel " .date("H:i:s");
+		echo " Hetkel on: " .$partOfDay . ".<p>";
+		
 	?>
+	<h2>Natuke vanusest</h2>
+	<form method="POST">
+		<label>Teie sünniaasta: </label>
+		<input name="birthYear" id="birthYear" type="number" value="<?php echo $myBirthYear?>" min="1900" max="2017">
+		<input name="submitBirthYear" type ="submit" value="Sisesta" >
+	</form>
+
+	<?php
+		if ($ageNotice != "") {
+			echo $ageNotice;
+		}
+	?>
+	
 </body>
 </html>
